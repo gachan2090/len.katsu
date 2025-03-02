fdd0c4cba6b54835ca7fccc7a2f46a1e7a44d0d1
gachan2090 <165966324+gachan2090@users.noreply.github.com>
2025-03-01 17:29:18 -0700

    created babel and main files

--- main.jsx
+++ main.jsx
@@ -1,16 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-function HelloWorld() {
-  return <h1 className="greeting">Hello, world!</h1>;
+function App() {
+    return <h1>Hello, JSX!</h1>;
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <HelloWorld />
-  </React.StrictMode>
-);
+root.render(<App />);
