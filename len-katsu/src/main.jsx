a625220ec12bd87d3c251b5e7316f02458da9a66
gachan2090 <165966324+gachan2090@users.noreply.github.com>
2025-03-02 16:22:56 -0700

    added title banner and timeline divs

--- len-katsu/src/main.jsx
+++ len-katsu/src/main.jsx
@@ -1,10 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import './index.css'
 
-function Hello(props) {
-  return <h1>Hello World!</h1>;
-}
+function TitleBanner() {
+  return <div id = "BannerContainer" class = "fadeIn">
+      <h1 id = "titleText">
+        Welcome to Garlen's Portfolio!
+        </h1> 
+    </div>
+};
+
+function VerticalLine(){
+  return <div id = "divWrapper"><div id = 'timelineBase' class = "fadeInSlow">
+  </div></div>
+};
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
-root.render(<Hello />);
+root.render(
+<div>
+  <TitleBanner />
+  <VerticalLine />
+  </div>
+);
