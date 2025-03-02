d321305fc1159831a8a326a9f3b72fc86912745e
gachan2090 <165966324+gachan2090@users.noreply.github.com>
2025-03-02 08:25:21 -0700

    updated main jsx

--- len-katsu/src/main.jsx
+++ len-katsu/src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+function Hello(props) {
+  return <h1>Hello World!</h1>;
+}
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(<Hello />);
