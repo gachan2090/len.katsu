d0570fd8024150664828e4d82084969d815794ee
gachan2090 <garlenchan1@gmail.com>
2025-03-09 10:57:58 -0600

    reformatted code to more appropriately set HTML

--- src/main.jsx
+++ src/main.jsx
@@ -1,83 +1,80 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 import ReactDOM from "react-dom/client";
-import './index.css';
-import gradPhoto from './assets/grad_photo.jpg';
+import "./index.css";
+import gradPhoto from "./assets/grad_photo.jpg";
 
-function TitleBanner() {
-  return <div id = "BannerContainer" class = "fadeIn">
-      <h1 id = "titleText">
-        Garlen (嘉麟) Chan
-        </h1> 
-    </div>
-};
+function FadeInSection({ children }) {
+  const [isVisible, setVisible] = useState(false);
+  const domRef = useRef();
 
-function FadeInSection(props) {
-  const [isVisible, setVisible] = React.useState(false);
-  const domRef = React.useRef();
-  React.useEffect(() => {
-    const observer = new IntersectionObserver(entries => {
-      entries.forEach(entry => setVisible(entry.isIntersecting));
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => entry.isIntersecting && setVisible(true));
     });
     observer.observe(domRef.current);
 
     return () => observer.unobserve(domRef.current);
   }, []);
+
   return (
-    <div
-      className={`fade-in-section ${isVisible ? 'is-visible' : ''}`}
-      ref={domRef}
-    >
-      {props.children}
+    <div className={`fade-in-section ${isVisible ? "is-visible" : ""}`} ref={domRef}>
+      {children}
     </div>
   );
 }
 
-function VerticalLine(){
+function App() {
   return (
-  <div id = "divWrapper">
-    <div id = 'timelineBase' class = "fadeInSlow">
-    <FadeInSection>
-      <div id="ProjectTBDBDashboard" >
-        <h1>Insert usage gif here #1</h1>
-      </div>
-    </FadeInSection>
-    <FadeInSection>
-      <div id="EMApp" >
-        <h1>Insert usage gif here #2</h1>
-      </div>
-    </FadeInSection>
-  </div>
-</div>)
-};
+    <div>
+      <header id="BannerContainer" className="fadeIn">
+        <h1 id="titleText">
+          Garlen (嘉麟) Chan's <br></br>
+          Gallery
+        </h1>
+      </header>
 
-function PersonalBio(){
-  return (<div id = "WholeBioWrapper" class="fadeInKindaSlow">
-    <div id="ImageWrapper">
-    <img src={gradPhoto} id = "BioImage"></img></div>
-    <div id = "BioTextWrapper">
-      <p id = "BioText">
-        Hello! My name is Garlen and I am a Process Engineer at 
-        Intel Corporation! <br></br><br></br>During my time as a PE, I slowly learned 
-        my passion for data analytics and data engineering, having built
-        data pipelines for my team to promote engineering efficiency in 
-        correct decision making. Utilizing my SQL and Python skills, I was
-        able to build dashboards,PyQt applets, and metrics that encouraged
-        teams to be more efficient with data analysis. <br></br><br></br><br></br>
-        In my freetime, I enjoy creating new things, and this portfolio built
-        on React is one of these hobbyist creations. Please scroll down and 
-        learn more of the work that I have completed so far!  
-      </p>
+      <main>
+        <section id="WholeBioWrapper" className="fadeInKindaSlow">
+          <div id="ImageWrapper">
+            <img src={gradPhoto} id="BioImage" alt="Graduation" />
+          </div>
+          <div id="BioTextWrapper">
+            <p id="BioText">
+              Hello! My name is Garlen and I am a Process Engineer at Intel Corporation.
+              <br />
+              <br />
+              Utilizing Python, SQL, and Power BI, I developed a passion for data analytics
+              and data science. In my free time, I utilize React to build other passion
+              projects, like this project gallery. Feel free to scroll and see my works!
+            </p>
+          </div>
+        </section>
 
+        <section id="divWrapper">
+          <div id="timelineLayerWrapper">
+            <FadeInSection>
+              <div id="timelineLayer1" className="projShowcase">
+                <h1>stuff1</h1>
+              </div>
+            </FadeInSection>
+            <FadeInSection>
+              <div id="timelineLayer2" className="projShowcase">
+                <h1>stuff2</h1>
+              </div>
+            </FadeInSection>
+            <FadeInSection>
+              <div id="timelineLayer3" className="projShowcase">
+                <h1>stuff3</h1>
+              </div>
+            </FadeInSection>
+          </div>
+          <div id="timelineBase" className="fadeInSlow"></div>
+        </section>
+      </main>
     </div>
-  </div>)
-};
+  );
+}
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
-root.render(
-<div>
-  <TitleBanner />
-  <PersonalBio />
-  <VerticalLine />
-  </div>
-);
+root.render(<App/>);
