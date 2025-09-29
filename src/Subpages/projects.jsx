e0b1af33156f2b15d1d3d76ed8633d9fabf77edf
gachan2090 <garlenchan1@gmail.com>
2025-09-28 22:18:27 -0700

    projects page started

--- src/Subpages/projects.jsx
+++ src/Subpages/projects.jsx
@@ -1,11 +1,57 @@
 import {Link} from "react-router-dom";
+import React, { useState, useRef, useEffect } from "react";
+import "./projects.css"
 
-const Projects = () => {
+const rnnProjectLink = "https://github.com/gachan2090/scratch-RNN"
+const rnnProjHeader = "ScratchRNN"
+const rnnText = "testtesttesttesttesttesttesttesttesttesttesttest\ntesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest\ntesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest\ntesttesttesttesttesttesttesttesttesttest"
+
+const recReaderProjectLink = 'https://github.com/gachan2090/rec-reader-grapher'
+const recReaderProjHeader = "RecRead"
+const recReaderText = "testtesttesttesttesttesttesttesttesttesttesttest\ntesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest\ntesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest\ntesttesttesttesttesttesttesttesttesttest"
+
+const imageBaseLink = "https://github.com/gachan2090/ImageBase"
+const imageBaseHeader = "ImageBase"
+const imageBaseText = "testtesttesttesttesttesttesttesttesttesttesttest\ntesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest\ntesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest\ntesttesttesttesttesttesttesttesttesttest"
+
+const debondModuleLink = "https://github.com/gachan2090/scratch-RNN"
+const debondModuleHeader = "Debond Dashboard"
+const debondModuleText = "testtesttesttesttesttesttesttesttesttesttesttest\ntesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest\ntesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest\ntesttesttesttesttesttesttesttesttesttest"
+
+const palantirFoundryLink = "https://github.com/gachan2090/scratch-RNN"
+const palantirFoundryHeader = "Palantir Foundry"
+const palantirFoundryText = "testtesttesttesttesttesttesttesttesttesttesttest\ntesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest\ntesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest\ntesttesttesttesttesttesttesttesttesttest"
+
+function Something(props){
     return (
-        <div>
-            <h1>Projects Page</h1>
-        <Link to="/">Go Home</Link>
+        <Link to={props.link} className="link-styles" id="projectsCard">
+            <h1 className="cardHeader">
+                {props.headers}
+            </h1>
+            <p className="cardDescription">
+                {props.text}
+            </p>
+        </Link>
+        )
+}
+
+function Projects() {
+    useEffect(() => {
+        setTimeout(() => {
+        window.scrollTo(0, 0);
+        }, 100);
+    }, []);
+
+    return (
+        <>
+        <div className="cardWrap">
+            <Something link={rnnProjectLink} headers={rnnProjHeader} text={rnnText} />
+            <Something link={recReaderProjectLink} headers={recReaderProjHeader} text={recReaderText} />
+            <Something link={imageBaseLink} headers={imageBaseHeader} text={imageBaseText} />
+            <Something link={debondModuleLink} headers={debondModuleHeader} text={debondModuleText} />
+            <Something link={palantirFoundryLink} headers={palantirFoundryHeader} text={palantirFoundryText} />
         </div>
+        </>
     );
 };
 
