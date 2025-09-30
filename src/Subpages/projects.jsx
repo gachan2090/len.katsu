5e9ee1defb72a0d2b4ad1e63ebc382b15fbd0de5
gachan2090 <garlenchan1@gmail.com>
2025-09-29 19:42:31 -0700

    configured projects page layout

--- src/Subpages/projects.jsx
+++ src/Subpages/projects.jsx
@@ -1,34 +1,39 @@
 import {Link} from "react-router-dom";
 import React, { useState, useRef, useEffect } from "react";
+import rnnProjectImg from "../assets/scratch rnn showcase.png"
+import recReadImg from "../assets/rec file reader.png"
 import "./projects.css"
 
 const rnnProjectLink = "https://github.com/gachan2090/scratch-RNN"
 const rnnProjHeader = "ScratchRNN"
-const rnnText = "testtesttesttesttesttesttesttesttesttesttesttest\ntesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest\ntesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest\ntesttesttesttesttesttesttesttesttesttest"
+const rnnText = "This is the description for my Recurrent Neural Network (RNN) project using pure NumPy, Pandas, and SciPy."
 
 const recReaderProjectLink = 'https://github.com/gachan2090/rec-reader-grapher'
 const recReaderProjHeader = "RecRead"
-const recReaderText = "testtesttesttesttesttesttesttesttesttesttesttest\ntesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest\ntesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest\ntesttesttesttesttesttesttesttesttesttest"
+const recReaderText = "This is the description for my Recurrent Neural Network (RNN) project using pure NumPy, Pandas, and SciPy."
 
 const imageBaseLink = "https://github.com/gachan2090/ImageBase"
 const imageBaseHeader = "ImageBase"
-const imageBaseText = "testtesttesttesttesttesttesttesttesttesttesttest\ntesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest\ntesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest\ntesttesttesttesttesttesttesttesttesttest"
+const imageBaseText = "This is the description for my Recurrent Neural Network (RNN) project using pure NumPy, Pandas, and SciPy."
 
 const debondModuleLink = "https://github.com/gachan2090/scratch-RNN"
 const debondModuleHeader = "Debond Dashboard"
-const debondModuleText = "testtesttesttesttesttesttesttesttesttesttesttest\ntesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest\ntesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest\ntesttesttesttesttesttesttesttesttesttest"
+const debondModuleText = "This is the description for my Recurrent Neural Network (RNN) project using pure NumPy, Pandas, and SciPy."
 
 const palantirFoundryLink = "https://github.com/gachan2090/scratch-RNN"
 const palantirFoundryHeader = "Palantir Foundry"
-const palantirFoundryText = "testtesttesttesttesttesttesttesttesttesttesttest\ntesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest\ntesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest\ntesttesttesttesttesttesttesttesttesttest"
+const palantirFoundryText = "This is the description for my Recurrent Neural Network (RNN) project using pure NumPy, Pandas, and SciPy."
 
 function Something(props){
     return (
-        <Link to={props.link} className="link-styles" id="projectsCard">
-            <h1 className="cardHeader">
-                {props.headers}
-            </h1>
-            <p className="cardDescription">
+        <Link to={props.link} className="link-styles-projects" id="projectsCard">
+            <span className="header-and-image">
+                <h1 className="cardHeader-projects">
+                    {props.headers}
+                </h1>
+                <img src={props.image}></img>
+            </span>
+            <p className="cardDescription-projects">
                 {props.text}
             </p>
         </Link>
@@ -44,12 +49,12 @@ function Projects() {
 
     return (
         <>
-        <div className="cardWrap">
-            <Something link={rnnProjectLink} headers={rnnProjHeader} text={rnnText} />
-            <Something link={recReaderProjectLink} headers={recReaderProjHeader} text={recReaderText} />
-            <Something link={imageBaseLink} headers={imageBaseHeader} text={imageBaseText} />
-            <Something link={debondModuleLink} headers={debondModuleHeader} text={debondModuleText} />
-            <Something link={palantirFoundryLink} headers={palantirFoundryHeader} text={palantirFoundryText} />
+        <div className="cardWrap-projects">
+            <Something link={rnnProjectLink} headers={rnnProjHeader} text={rnnText} image={rnnProjectImg}/>
+            <Something link={recReaderProjectLink} headers={recReaderProjHeader} text={recReaderText} image={recReadImg}/>
+            <Something link={imageBaseLink} headers={imageBaseHeader} text={imageBaseText} image={rnnProjectImg}/>
+            <Something link={debondModuleLink} headers={debondModuleHeader} text={debondModuleText} image={rnnProjectImg}/>
+            <Something link={palantirFoundryLink} headers={palantirFoundryHeader} text={palantirFoundryText} image={rnnProjectImg}/>
         </div>
         </>
     );
