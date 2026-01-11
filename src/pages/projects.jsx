22633266c337e4d343cc205e53a0931794facb32
gachan2090 <garlenchan1@gmail.com>
2026-01-10 16:23:41 -0800

    Update projects.jsx

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -14,17 +14,17 @@ const rnnText = "Built a generative AI model for predictive temperature modeling
 
 const recReaderProjectLink = 'https://github.com/gachan2090/rec-reader-grapher'
 const recReaderProjHeader = "RecRead"
-const recReaderText = "Developed a Python-based application to analyze factory production .rec files using Object-Oriented-Programming (OOP), PyQTConfig, NumPy, Pandas, and Matplotlib. Streamlined statistical analysis and automated graph generation, building upon human-centered design principles and reducing engineering time by 20 hours per week and saving ~$1.1M annually."
+const recReaderText = "Developed a Python-based application to analyze factory data using object oriented programming (OOP). Streamlined statistical analysis and automated graph generation, building upon human-centered design principles and saving engineering time by 20 hours per week cumulatively."
 
 const imageBaseLink = "https://github.com/gachan2090/ImageBase"
 const imageBaseHeader = "ImageBase"
 const imageBaseText = "Created a full-stack home server for photo backup to reduce reliance on cloud storage. Built with PostgreSQL (metadata), Django REST API (backend), and ReactTS (frontend). Learned database design, API development, system architecture, and intranet security."
 
-const debondModuleLink = "https://github.com/gachan2090/scratch-RNN"
+const debondModuleLink = "https://github.com/gachan2090"
 const debondModuleHeader = "Debond Dashboard"
-const debondModuleText = "Designed a centralized Power BI dashboard integrating ERP, MES, SPC, and quality systems. Delivered actionable performance metrics and quarterly trends, reducing SPC flagging by 90%, boosting equipment availability by 55%, and increasing production run rates by 50%. Resulted in $3.5M+ savings annually."
+const debondModuleText = "Designed a centralized Power BI dashboard integrating ERP, MES, SPC, and quality systems. Delivered actionable performance metrics and quarterly trends, reducing SPC flagging by 80%, boosting equipment availability by 55%, and increasing production run rates by 50%. Resulted in $3.5M+ savings annually."
 
-const palantirFoundryLink = "https://github.com/gachan2090/scratch-RNN"
+const palantirFoundryLink = "https://github.com/gachan2090"
 const palantirFoundryHeader = "Test Analytics"
 const palantirFoundryText = "Designed and implemented views quantifying key performance indicators (KPIs) for manufacturing operations using Palantir Foundry. Automated data pipelines and created interactive dashboards, enabling insights into test data and creating actionable recommendations for process changes, such as control capability (CpK) improvement and test value distribution centralization within the 3-sigma standard."
 
